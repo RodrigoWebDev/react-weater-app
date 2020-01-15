19143bcf29b6ef70203f8ea354278641c43d3368
Rodrigo Queiroz <rodrigo.queiroz.chagas@gmail.com>
2020-01-15 11:56:27 -0300

    Requisição na API do openwheater

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import ajax from '@fdaciuk/ajax'
+import axios from "axios"
 import './App.css';
 
 /*
@@ -7,73 +7,126 @@ import './App.css';
 */
 
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       city: "",
       weatherInfo: null,
-      isFetching: false
+      isFetching: false,
+      fetch: false,
+      error: false
     }
-  }
 
-  openWeatherApi(city){
-    return `api.openweathermap.org/data/2.5/forecast?q=${city}`;
-  }
+    this.handleChange = (e) => {
+      console.log("handleChange");
+      const value = e.target.value;
 
-  handleSearch(e){
-    e.preventDefault();
-    const value = e.target.value;
+      this.setState({
+        city: value
+      })
+    }
 
-    this.setState({
-      city: value
-    })
+    this.openWeatherApi = (city) => {
+      const apiKey = "7664f5403c235171315453a76f72e8d8";
+      return `http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`;
+    }
+
+    this.handleClick = () => {
+      const value = this.state.city
+      const self = this;
+
+      self.setState({ isFetching: true, error: false });
+
+      axios.get(this.openWeatherApi(value))
+      .then(function(res){
+        console.log("response > ", res);
+        
+        let main = res.data.list[0].main
+        let city = res.data.city
+
+        self.setState({ 
+          isFetching: false, 
+          fetch: true ,
+          weatherInfo: {
+            temp: main.temp,
+            temp_min: main.temp_min,
+            temp_max: main.temp_max,
+            wheater: res.data.list[0].weather[0].description,
+            humidity: main.humidity,
+            sunrise: city.sunrise,
+            sunset: city.sunset,
+            date: res.data.list[0].dt_txt
+          }
+        });
+      })
+      .catch(function(err){
+        console.log("error > ", err);
+        self.setState({ isFetching: false, error: true });
+      })
+      .finally(function(){
+      })
+    }
   }
 
   render() {
     return (
       <div className="App">
-        <form>
-          <input type="text" placeholder="Busca por cidade" />
-          <button className="button">Procurar</button>
-        </form>
-        <div className="lds-ring"><div></div><div></div><div></div><div></div></div>
+        {!this.state.fetch &&
+          <div class="search">
+              <input onChange={this.handleChange} type="text" placeholder="Busca por cidade" />
+              <button onClick={this.handleClick} className="button" type="submit">Procurar</button>
+          </div>
+        }
 
-        <div className="tempo">
-          <h2 className="cidade">{this.state.city}</h2>
-          <h3>Dia da semana 31 de Mês de 2019</h3>
+        {this.state.isFetching &&
+          <div className="lds-ring"><div></div><div></div><div></div><div></div></div>
+        }
 
-          <div className="tempo-content">
-            <div>
-              <div>Mínima</div>
-              <span>{this.state.weatherInfo.temp_min}</span>
+        {this.state.fetch &&
+          <div className="wheater">
+            <h2 className="city">{this.state.city}</h2>
+            <h3>{this.state.weatherInfo.date}</h3>
+
+            <div className="whater-content">
+              <div>
+                <div>Mínima</div>
+                <span>{this.state.weatherInfo.temp_min}</span>
+              </div>
+              <div>
+                <div>{this.state.weatherInfo.temp}</div>
+                <span>{this.state.weatherInfo.wheater}</span>
+              </div>
+              <div>
+                <div>Mínima</div>
+                <span>{this.state.weatherInfo.temp_max}</span>
+              </div>
             </div>
-            <div>
-              <div>{this.state.weatherInfo.temp}</div>
-              <span>Parcialmente Nublado</span>
-            </div>
-            <div>
-              <div>Mínima</div>
-              <span>{this.state.weatherInfo.temp_max}</span>
+
+            <div class="wheater-footer">
+              <div>
+                <span>Umidade</span>
+                <span>{this.state.weatherInfo.humidity}</span>
+              </div>
+
+              <div>
+                <span>Nascer do Sol</span>
+                <span>{this.state.weatherInfo.sunrise}</span>
+              </div>
+
+              <div>
+                <span>Pôr do sol</span>
+                <span>{this.state.weatherInfo.sunset}</span>
+              </div>
             </div>
           </div>
+        }
 
-          <div class="tempo-footer">
-            <div>
-              <span>Umidade</span>
-              <span>20%</span>
-            </div>
-
-            <div>
-              <span>Nascer do Sol</span>
-              <span>6:30</span>
-            </div>
-
-            <div>
-              <span>Pôr do sol</span>
-              <span>20:10</span>
-            </div>
+        {this.state.error && 
+        <div className="error">
+          <h2>Ops! Ocorreu um erro. Por favor, tente novamente</h2>
           </div>
-        </div>
+        }
+
       </div>
     );
   }
